2e86335abd5dd1912b786a8c64533cad00cb9de1
Qinyuan Liu <liu.qinyuan@teamspirit.com>
2024-09-06 11:56:34 +0800

    Add initial balance and date, fix styling

--- my-app/src/components/MainTable.tsx
+++ my-app/src/components/MainTable.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableCell, { tableCellClasses, TableCellProps } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
@@ -10,12 +10,29 @@ import TextField, { TextFieldProps } from "@mui/material/TextField";
 import Input from "@mui/material/Input";
 import "./MainTable.scss";
 import { styled } from "@mui/material/styles";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Dayjs } from "dayjs";
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
+interface Row {
+  withdraw: number;
+  depo: number;
+  balance: number;
+  adb: number;
 }
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
+interface FixedWidthTableCellProps extends TableCellProps {
+  fixedWidth?: number; // Optional custom prop for fixed width
+}
+
+type State = Array<Row>;
+
+const StyledTableCell = styled(TableCell)<FixedWidthTableCellProps>(({ theme, fixedWidth }) => ({
+  width: fixedWidth ? `${fixedWidth}px` : "atuo",
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
@@ -24,6 +41,8 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     fontSize: 14,
   },
 }));
+
+const VALUE_DATE_WIDTH = 160;
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
@@ -36,14 +55,8 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 const MAX_DATE = 31;
+const MAX_NUM = 1000000000;
 
-// const getRows = () => {
-//   const rows = [...Array(MAX_DATE)].map((_, idx) => {
-//     return { date: idx + 1, withdraw: 1000, depo: 2000, balance: 200, adb: 300 };
-//   });
-//   console.log(rows);
-//   return rows;
-// };
 const formatNumber = (num: number) => {
   return num.toFixed(2);
 };
@@ -52,15 +65,6 @@ const commonInputProps: Partial<TextFieldProps> = { size: "small", variant: "out
 
 const ROOT = "adb-table";
 
-interface Row {
-  withdraw: number;
-  depo: number;
-  balance: number;
-  adb: number;
-}
-
-type State = Array<Row>;
-
 const MainTable = () => {
   const initialRows = [...Array(MAX_DATE)].map((_, idx) => {
     return { withdraw: 0, depo: 0, balance: 0, adb: 0 };
@@ -68,9 +72,11 @@ const MainTable = () => {
 
   const [rows, setRows] = useState<State>(initialRows);
   const [initialBalance, setInitialBalance] = useState<number>(0);
+  const [initialDate, setInitialDate] = useState<Dayjs | null>(null);
 
   const handleChangeWithDr = (value: string, index: number) => {
     const val = Number(value);
+    if (val > MAX_NUM) return;
     setRows((prevRows) => {
       const rows = [] as State;
       let total = 0;
@@ -89,13 +95,13 @@ const MainTable = () => {
           rows.push({ withdraw: newWithdraw, depo: newDepo, balance: newBalance, adb: newAdb });
         }
       });
-      console.log(rows);
       return rows;
     });
   };
 
   const handleChangeDepo = (value: string, index: number) => {
     const val = Number(value);
+    if (val > MAX_NUM) return;
     setRows((prevRows) => {
       const rows = [] as State;
       let total = 0;
@@ -114,7 +120,6 @@ const MainTable = () => {
           rows.push({ withdraw: newWithdraw, depo: newDepo, balance: newBalance, adb: newAdb });
         }
       });
-      console.log(rows);
       return rows;
     });
   };
@@ -133,21 +138,58 @@ const MainTable = () => {
         const newAdb = total / (idx + 1);
         rows.push({ withdraw: row.withdraw, depo: row.depo, balance: newBalance, adb: newAdb });
       });
-      console.log(rows);
       return rows;
     });
+  };
+
+  const handleChangeInitDate = (value: Dayjs | null) => {
+    setInitialDate(value);
   };
 
   return (
     <div className={ROOT}>
       <div className={`${ROOT}-title`}>Average Daily Balance (ADB) Calculator</div>
+
+      <div className={`${ROOT}-init`}>
+        <div className={`${ROOT}-init-balance`}>
+          <div className={`${ROOT}-init-label`}>Initial Balance</div>
+          <TextField
+            {...commonInputProps}
+            id="standard-basic"
+            onBlur={handleChangeInitialBalance}
+            size="medium"
+            InputProps={{
+              inputProps: { style: { textAlign: "right" } },
+            }}
+            type="number"
+          />
+        </div>
+
+        <div className={`${ROOT}-init-day`}>
+          <div className={`${ROOT}-init-label`}>First Day to start with</div>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker onChange={handleChangeInitDate} value={initialDate} />
+          </LocalizationProvider>
+        </div>
+      </div>
+
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table" stickyHeader>
           <TableHead>
             <StyledTableRow>
-              <StyledTableCell>Value Date</StyledTableCell>
-              <StyledTableCell align="right">Withdrawal</StyledTableCell>
-              <StyledTableCell align="right">Deposit</StyledTableCell>
+              <StyledTableCell fixedWidth={VALUE_DATE_WIDTH}>Value Date</StyledTableCell>
+              <StyledTableCell align="right">
+                <div className={`${ROOT}-th`}>
+                  <RemoveIcon />
+                  <span>Withdrawal</span>
+                </div>
+              </StyledTableCell>
+              <StyledTableCell align="right">
+                <div className={`${ROOT}-th`}>
+                  <AddIcon />
+                  <span>Deposit</span>
+                </div>
+              </StyledTableCell>
               <StyledTableCell align="right">Balance as of the day</StyledTableCell>
               <StyledTableCell align="right">ADB as of the day</StyledTableCell>
             </StyledTableRow>
@@ -155,12 +197,12 @@ const MainTable = () => {
           <TableBody>
             {rows.map((row, index) => (
               <StyledTableRow key={index} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                <StyledTableCell component="th" scope="row">
-                  <div className={`${ROOT}-day-col`}>
-                    <div className={`${ROOT}-day`}>
-                      <div>Day</div> <div className={`${ROOT}-day-num`}>{index + 1}</div>
+                <StyledTableCell component="th" scope="row" fixedWidth={VALUE_DATE_WIDTH}>
+                  <div className={`${ROOT}-value-date`}>
+                    <div className={`${ROOT}-value-date-day`}>
+                      <div>Day</div> <div className={`${ROOT}-value-date-day-num`}>{index + 1}</div>
                     </div>
-                    {index === 0 && <TextField {...commonInputProps} id="standard-basic" label="Initial Balance" onBlur={handleChangeInitialBalance} />}
+                    {initialDate?.isValid() && <span className={`${ROOT}-value-date-date`}>{initialDate.add(index, "day").toDate().toLocaleDateString()}</span>}
                   </div>
                 </StyledTableCell>
                 <StyledTableCell align="right">
@@ -173,8 +215,9 @@ const MainTable = () => {
                       handleChangeWithDr(e.target.value, index);
                     }}
                     InputProps={{
-                      inputProps: { style: { textAlign: "right" } }, // Aligns the input text to the right
+                      inputProps: { style: { textAlign: "right" } },
                     }}
+                    value={row.withdraw || ""}
                   />
                 </StyledTableCell>
                 <StyledTableCell align="right">
@@ -189,6 +232,7 @@ const MainTable = () => {
                     InputProps={{
                       inputProps: { style: { textAlign: "right" } }, // Aligns the input text to the right
                     }}
+                    value={row.depo || ""}
                   />
                 </StyledTableCell>
                 <StyledTableCell align="right">{formatNumber(row.balance)}</StyledTableCell>
